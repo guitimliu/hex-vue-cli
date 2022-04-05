8e00d3ccd28fdfe435117293008321691e82a0ec
Gui <guitimliu@gmail.com>
2022-04-06 01:13:44 +0800

    商品列表頁與分頁元件

---
+++ src/components/PaginationButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li class="page-item" v-if="data?.has_pre">
+                <button type="button" class="page-link" @click="changePage(data?.current_page-1)">
+                    上一頁
+                </button>
+            </li>
+            <template v-for="page in data?.total_pages" :key="page">
+                <li v-if="page != data?.current_page" class="page-item">
+                    <button type="button" class="page-link" @click="changePage(page)">
+                        {{ page }}
+                    </button>
+                </li>
+                <li v-else class="page-item disabled">
+                    <button type="button" class="page-link" @click="changePage(page)">
+                        {{ page }}
+                    </button>
+                </li>
+            </template>
+            <li class="page-item" v-if="data?.has_next">
+                <button type="button" class="page-link" @click="changePage(data?.current_page+1)">
+                    下一頁
+                </button>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    changePage (page) {
+      this.$emit('return-page', page)
+    }
+  },
+  props: ['data']
+}
+</script>
---
+++ src/views/admin/AdminProducts.vue
@@ -0,0 +1,74 @@
+<template>
+    <button type="button" v-on:click="showDetail()" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#productDetail">
+        新增商品
+    </button>
+    <table class="table table-hover table-striped table-borderless shadow" data-aos="zoom-in">
+        <thead>
+            <tr>
+                <th class="percenTwenly">產品名稱</th>
+                <th class="percenTwenly">原價</th>
+                <th class="percenTwenly">售價</th>
+                <th class="percenTwenly">是否啟用</th>
+                <th class="percenTwenly">查看細節</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- 商品清單 -->
+            <tr v-for="item in products" :key="item">
+            <td>{{ item.title }}</td>
+            <td><del>{{ item.origin_price }}</del></td>
+            <td>{{ item.price }}</td>
+            <td>
+                <span class="rounded-2 bg-success p-1" v-if="item.is_enabled">啟用</span>
+                <span class="rounded-2 bg-danger p-1" v-else>未啟用</span>
+            </td>
+            <td>
+                <button type="button" v-on:click="showDetail(item)" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#productDetail">
+                    編輯商品
+                </button>
+                <!-- <button type="button" @click="deleteProduct(item)" class="btn btn-danger ms-2">刪除商品</button> -->
+                <button type="button" class="btn btn-danger ms-2" @click="temp = {...item}" data-bs-toggle="modal" data-bs-target="#deleteModal">刪除商品</button>
+            </td>
+            </tr>
+        </tbody>
+    </table>
+    <Pagination :data="pagination" @return-page="getProducts" />
+</template>
+
+<script>
+import axios from 'axios'
+import checkLogin from '@/assets/script/checkLogin'
+import Pagination from '@/components/PaginationButton.vue'
+
+export default {
+  components: {
+    Pagination
+  },
+  data () {
+    return {
+      products: null,
+      pagination: null
+    }
+  },
+  methods: {
+    getProducts (page = 1) {
+      axios.get(`${process.env.VUE_APP_API_URL}api/${process.env.VUE_APP_API_PATH}/admin/products?page=${page}`)
+        .then(res => {
+          this.products = res.data.products
+          this.pagination = res.data.pagination
+        })
+        .catch(() => { /* error */ })
+    }
+  },
+  mounted () {
+    checkLogin()
+    this.getProducts()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.percenTwenly {
+    width: 20%;
+}
+</style>
