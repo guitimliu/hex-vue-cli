3811a36192bd00c123ded45380e170ec74b9a322
Gui <guitimliu@gmail.com>
2022-04-07 23:53:09 +0800

    後台訂單列表頁完成

--- src/views/admin/AdminOrders.vue
+++ src/views/admin/AdminOrders.vue
@@ -1,3 +1,52 @@
 <template>
-    訂單列表
+    <h1 class="h3">訂單列表</h1>
+    <table class="table table-hover table-striped table-borderless shadow" data-aos="zoom-in">
+        <thead>
+            <tr>
+                <th class="percenTwenly">訂單 ID</th>
+                <th class="percenTwenly">訂購人</th>
+                <th class="percenTwenly">訂單時間</th>
+                <th class="percenTwenly">價格</th>
+                <th class="percenTwenly">查看細節</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in orders" :key="item">
+                <td>{{ item.id }}</td>
+                <td>{{ item.user.name }}</td>
+                <td>{{ item.create_at }}</td>
+                <td>{{ item.total }}</td>
+            <td>
+                <button type="button" v-on:click="deliveryProduct({...item})" class="btn btn-secondary"  >
+                    編輯訂單
+                </button>
+                <button type="button" class="btn btn-danger ms-2" @click="temp = {...item}" data-bs-toggle="modal" data-bs-target="#deleteModal">刪除訂單</button>
+            </td>
+            </tr>
+        </tbody>
+    </table>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      orders: null
+    }
+  },
+  methods: {
+    getOrders (page = 1) {
+      axios.get(`${process.env.VUE_APP_API_URL}api/${process.env.VUE_APP_API_PATH}/admin/orders?page=${page}`)
+        .then(res => {
+          this.orders = res.data.orders
+        })
+        .catch(() => { /* error */ })
+    }
+  },
+  mounted () {
+    this.getOrders()
+  }
+}
+</script>
