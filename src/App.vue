377cf142fbc55e2618a9931b536e33c678d874fb
Gui <guitimliu@gmail.com>
2022-04-10 02:10:39 +0800

    h1 置中、其餘文字置左

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,13 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
 
 nav {
   padding: 30px;
+  text-align: center;
 
   a {
     font-weight: bold;
@@ -30,5 +31,9 @@ nav {
       color: #42b983;
     }
   }
+}
+
+h1 {
+  text-align: center;
 }
 </style>
