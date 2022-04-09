0310d65b562df67f61a375a527aa5c6d776c2613
Gui <guitimliu@gmail.com>
2022-04-10 03:38:19 +0800

    調整後台選單與主要內容格線占比 & 登出

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="list-group">
                     <template v-for="menu in adminMenus" :key="menu.name">
                         <router-link class="list-group-item" :to="menu.link">{{ menu.name }}</router-link>
                     </template>
+                    <router-link class="list-group-item text-start" to="" @click="logout">登出</router-link>
                 </div>
             </div>
-            <div class="col-md-8">
+            <div class="col-md-9">
                 <router-view></router-view>
             </div>
         </div>
@@ -16,6 +17,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import checkLogin from '@/assets/script/checkLogin'
 
 export default {
@@ -31,6 +33,16 @@ export default {
           link: '/admin/orders'
         }
       ]
+    }
+  },
+  methods: {
+    logout () {
+      axios.post('https://vue3-course-api.hexschool.io/v2/logout')
+        .then((res) => {
+          alert(res.data.message)
+          location.reload()
+        })
+        .catch(() => { /* error */ })
     }
   },
   mounted () {
